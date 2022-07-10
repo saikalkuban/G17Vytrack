de240a809808fd2f11dbc703b39af3112542431e
nadireirem <nadireulsular@gmail.com>
2022-07-10 16:11:13 -0400

    testing

--- src/test/java/com/vytrack/runner/CukesRunner.java
+++ src/test/java/com/vytrack/runner/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@gridSettings"
 
 )
 public class CukesRunner {
